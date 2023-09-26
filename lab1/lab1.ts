187a98fb364e516c3bbd2f41d1555a7a92ec0f0f
Alexander <123123salo21082018@gmail.com>
2023-09-26 17:38:08 +0300

    lab1

--- lab1/lab1.ts
+++ lab1/lab1.ts
@@ -1,51 +1,66 @@
 import fs from 'fs'
 import Jimp from 'jimp'
+import zlib from "zlib";
 
 // если .format === 'jpg', у объекта есть дополнительное числовое свойство quality
 // если .format === 'png', дополнительных свойств нет
 export type ConversionOptions =
-  | {format: 'jpg'; quality: 99 | 90 | 75 | 50}
-  | {format: 'png'}
-  | {format: 'deflate'}
+    | { format: 'jpg'; quality: 99 | 90 | 75 | 50 }
+    | { format: 'png' }
+    | { format: 'deflate' }
 
 export type CompressionInfo = {
-  size: number
-  compressedSize: number
-  compressionRatio: number
+    sizeBefore: number
+    sizeAfter: number
+    compressionRatio: number
 }
 
 export async function convert(filename: string, options: ConversionOptions): Promise<CompressionInfo> {
-  // пример загрузки изображения, конвертации в буфер и оценки размера
-  const bikeBmp = await Jimp.read(filename)
+    const bikeBmp = await Jimp.read(filename);
 
-  const bmpBuffer = await bikeBmp.getBufferAsync(Jimp.MIME_BMP)
-  const bmpSize = bmpBuffer.byteLength
+    const bmpBuffer = await bikeBmp.getBufferAsync(Jimp.MIME_BMP);
+    const bmpSize = bmpBuffer.byteLength;
 
-  const jpeg95Buffer = await bikeBmp.quality(95).getBufferAsync(Jimp.MIME_JPEG)
-  const jpeg95Size = jpeg95Buffer.byteLength
+    const compressionInfo: CompressionInfo = {
+        sizeBefore: bmpSize,
+        sizeAfter: 0,
+        compressionRatio: 0
+    };
 
-  // пример объекта, который нужно вернуть из этой функции:
-  const compressionInfo: CompressionInfo = {
-    size: bmpSize,
-    compressedSize: jpeg95Size,
-    compressionRatio: jpeg95Size / bmpSize,
-  }
+    let sizeAfter;
 
-  if (options.format === 'jpg') {
-    const {quality} = options
-    // TODO: исходный код к заданию 2 здесь, можно использовать число quality
-    throw new Error('Конвертация в JPG не реализована!')
-  } else if (options.format === 'png') {
-    // TODO: исходный код к заданию 2 здесь
-    throw new Error('Конвертация в PNG не реализована!')
-  } else {
-    // TODO: исходный код к заданию 2 здесь
-    // тут необходимо использовать функцию zlib.deflate+utils.promisify или zlib.deflateSync
-    throw new Error('Конвертация в Deflate не реализована!')
-  }
+    if (options.format === 'jpg') {
+        const {quality} = options;
+        const buffer = await bikeBmp.quality(quality).getBufferAsync(Jimp.MIME_JPEG);
+        sizeAfter = buffer.byteLength;
+    } else if (options.format === 'png') {
+        const buffer = await bikeBmp.getBufferAsync(Jimp.MIME_PNG);
+        sizeAfter = buffer.byteLength;
+    } else {
+        const zlib = require("zlib");
+        const deflate = zlib.deflateSync(bmpBuffer);
+        sizeAfter = deflate.buffer.byteLength;
+    }
+
+    compressionInfo.sizeAfter = sizeAfter;
+    compressionInfo.compressionRatio = sizeAfter / bmpSize;
+
+    return compressionInfo;
+
 }
 
 export async function pngAndDeflate(filename: string): Promise<CompressionInfo> {
-  // TODO: исходный код к заданию 4 здесь
-  throw new Error('Конвертация PNG+Deflate не реализована!')
+    const bmpFile = await Jimp.read(filename);
+    const bmpBuffer = await bmpFile.getBufferAsync(Jimp.MIME_BMP);
+    const bmpSize = bmpBuffer.byteLength;
+
+    const pngBuffer = await bmpFile.getBufferAsync(Jimp.MIME_PNG);
+    const newSize = zlib.deflateSync(pngBuffer).buffer.byteLength;
+
+    return {
+        sizeBefore: bmpSize,
+        sizeAfter: newSize,
+        compressionRatio: newSize / bmpSize
+    };
+
 }
